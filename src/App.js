3ff30563d08e7454cbebc8c59d6291fc31d3002b
Asliddin Azizovich <space.coding.programmer@gmail.com>
2020-08-03 18:06:45 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ListItem from './ListItem';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faTrash);
+
+class App extends React.Component {
+  // App constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+  }
+
+  // Add new item
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== '') {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          key: '',
+          text: ''
+        }
+      });
+    }
+  }
+
+  // Delete existing item
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(
+      item => item.key !== key
+    );
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  // Update value of item
+  updateItem(key, value) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key === key) {
+        return {
+          key: key,
+          text: value
+        }
+      }
+
+      return item
+    });
+
+    this.setState({
+      items: newItems
+    })
+  }
+
+  // Handle input
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  // Component rendering
+  render() {
+    return (
+      <div className="App" >
+        <header>
+          <form id="todoForm" onSubmit={(e) => this.addItem(e)}>
+            <input onChange={(e) => this.handleInput(e)} value={this.state.currentItem.text} type="text" placeholder="Enter Text" />
+            <button type="submit">Add</button>
+          </form>
+          <ListItem
+            items={this.state.items}
+            deleteItem={(key) => this.deleteItem(key)}
+            updateItem={(key, value) => this.updateItem(key, value)}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ListItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
+import './ListItem.css'
+
+const ListItem = (props) => {
+    const items = props.items;
+
+    const listItems = items.map(item => {
+        return (
+            <div className="list-item" key={item.key}>
+                <p>
+                    <input type="text"
+                        id={item.key}
+                        value={item.text}
+                        onChange={(e) => props.updateItem(item.key, e.target.value)}
+                    />
+                    <span>
+                        <FontAwesomeIcon
+                            className="faicons"
+                            icon="trash"
+                            onClick={() => props.deleteItem(item.key)}
+                        />
+                    </span>
+                </p>
+            </div>
+        )
+    });
+
+    return (
+        <div>
+            <FlipMove duration={300} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+        </div>
+    )
+}
+
+export default ListItem;
